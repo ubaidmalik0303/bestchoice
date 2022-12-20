8154401838ba7748c800c5dd45411712e4038b90
ubaidmalik0303 <ubaidmalik0303@gmail.com>
2022-12-20 14:27:59 +0500

    deployment changes

--- frontend/src/utils/AxiosInstance/index.js
+++ frontend/src/utils/AxiosInstance/index.js
@@ -1,6 +1,5 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "https://bestchoicee.herokuapp.com/",
-  // baseURL: "http://localhost:4000/",
+  baseURL: "http://localhost:4000/",
 });
